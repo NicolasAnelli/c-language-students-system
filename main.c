492e8a078336d94835061c8d4230a4185a2faef7
Nicolas Anelli <nickanelli@hotmail.com>
2019-05-28 16:33:44 -0300

    ignoring dados.dat

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /**
  * [V] - Cadastrar
  * [V] - Listar
- * [ ] - Aprovados
- * [ ] - Reprovados
+ * [V] - Aprovados
+ * [V] - Reprovados
  * [ ] - Salvar no arquivo
  * [ ] - Ler do arquivo
  * [V] - Menu
@@ -17,7 +17,7 @@
 #define PESO_N2 2
 #define TAMANHO_INICIAL_LISTA 20
 #define NOME_SISTEMA "Sistema de Controle de Alunos"
-#define DEBUG 1
+#define ARQ_LISTA "dados.dat"
 
 // Aluno
 typedef struct {
@@ -29,9 +29,9 @@ typedef struct {
 
 // Lista
 typedef struct {
-	Aluno *aluno;
 	int quantidade;
 	int maximo;
+	Aluno *aluno;
 } Lista;
 
 // Funcoes principais de sistema
@@ -39,6 +39,7 @@ Lista* criaLista(int quantidadeMaxima);
 Aluno* criaAluno(int codigo, char* nome, float nota1, float nota2);
 void adicionarAluno(Lista* lista, Aluno* aluno);
 float calculaMedia(Aluno* aluno);
+int estaAprovado(Aluno* aluno);
 char* resolveSituacao(Aluno* aluno);
 
 // Funcoes de tela e menus
@@ -56,6 +57,12 @@ void imprimeListaAlunos(int ini);
 void menuAprovados();
 void imprimeListaAprovados();
 
+void menuReprovados();
+void imprimeListaReprovados();
+
+// Funcoes de persistencia
+void lerDadosDoArquivo(char *filename);
+void salvarDadosNoArquivo(char* filename);
 
 // Funcoes de mensagem
 void setMensagemAviso(char *msg);
@@ -72,11 +79,20 @@ Lista* lista_;
 
 int main () {
 
-	if (DEBUG){
-		lista_ = criaListaFake();
-	} else {
-		lista_ = criaLista(TAMANHO_INICIAL_LISTA);
+	clearScreen();
+
+	char *opcao = (char*)malloc(sizeof(char));
+	printf("Execute esse programa no console 80x24\n");
+	printf("Continuar[s/N]: ");
+	scanf( "%c", opcao);
+	if(*opcao != 's' && *opcao != 'S'){
+		clearScreen();
+		free(opcao);
+		return EXIT_SUCCESS;
 	}
+	free(opcao);
+
+	lista_ = criaLista(TAMANHO_INICIAL_LISTA);
 
 	menuPrincipal();
 
@@ -116,16 +132,22 @@ void adicionarAluno(Lista* lista, Aluno* aluno) {
 float calculaMedia(Aluno* aluno) {
 	return (PESO_N1*aluno->nota1 + PESO_N2*aluno->nota2)/(PESO_N1+PESO_N2);
 }
-char* resolveSituacao(Aluno* aluno) {
-
+int estaAprovado(Aluno* aluno) {
 	float media = calculaMedia(aluno);
 	if (media >= 7) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+char* resolveSituacao(Aluno* aluno) {
+
+	if (estaAprovado(aluno)) {
 		return "Aprovado";
 	} else {
 		return "Reprovado";
 	}
 }
-
 
 // Funcoes de tela e menus
 void cabecalho(char* menu) {
@@ -159,9 +181,35 @@ void menuPrincipal(){
 				break;
 
 			case '3':
+				clearScreen();
+				menuAprovados();
 				break;
 
 			case '4':
+				clearScreen();
+				menuReprovados();
+				break;
+
+			case '5':
+				clearScreen();
+				lerDadosDoArquivo(ARQ_LISTA);
+				break;
+
+			case '6':
+				clearScreen();
+				salvarDadosNoArquivo(ARQ_LISTA);
+				break;
+
+			case '7':
+				clearScreen();
+				free(lista_);
+				lista_ = criaLista(TAMANHO_INICIAL_LISTA);
+				break;
+
+			case 'f':
+			case 'F':
+				free(lista_);
+				lista_ = criaListaFake();
 				break;
 
 			case 'd':
@@ -171,6 +219,7 @@ void menuPrincipal(){
 				printf("Digite qualquer para voltar: ");
 				scanf(" %c", opcao);
 				break;
+
 			case '0':
 				clearScreen();
 				free(opcao);
@@ -180,19 +229,19 @@ void menuPrincipal(){
 }
 void imprimeMenuPrincipal() {
 	cabecalho("Menu principal");
-	emptyLine();
 	line("1. Cadastrar", 'L');
 	emptyLine();
 	line("2. Lista", 'L');
 	emptyLine();
+	line("3. Aprovados", 'L');
 	emptyLine();
+	line("4. Reprovados", 'L');
 	emptyLine();
+	line("5. Ler do arquivo", 'L');
 	emptyLine();
+	line("6. Salvar no arquivo", 'L');
 	emptyLine();
-	emptyLine();
-	emptyLine();
-	emptyLine();
-	emptyLine();
+	line("7. Restaurar (Nao salva automaticamente)", 'L');
 	emptyLine();
 	emptyLine();
 	line("0. Sair", 'L');
@@ -273,7 +322,6 @@ void imprimeCadastroAluno(char* nome, float nota1, float nota2) {
 	char *aux = (char*)malloc(80*sizeof(char));
 
 	cabecalho("Cadastro de alunos");
-	emptyLine();
 	sprintf(aux, "1. Nome:  %s", nome);
 	line(aux, 'L');
 	emptyLine();
@@ -284,6 +332,7 @@ void imprimeCadastroAluno(char* nome, float nota1, float nota2) {
 	line(aux, 'L');
 	emptyLine();
 	line("4. Salvar", 'L');
+	emptyLine();
 	emptyLine();
 	emptyLine();
 	emptyLine();
@@ -339,8 +388,8 @@ void imprimeListaAlunos(int ini){
 	cabecalho("Lista de alunos");
 
 	for (i=ini; i<ini+4; i++) {
-		emptyLine();
-		if(!strcmp(lista_->aluno[i].nome, "")) {
+		if(i >= lista_->quantidade) {
+			emptyLine();
 			emptyLine();
 			emptyLine();
 		} else {
@@ -353,6 +402,7 @@ void imprimeListaAlunos(int ini){
 						lista_->aluno[i].nota2,
 						calculaMedia(&lista_->aluno[i]));
 			line(aux, 'C');
+			emptyLine();
 		}
 	}
 
@@ -368,11 +418,197 @@ void imprimeListaAlunos(int ini){
 
 void menuAprovados(){
 
+	int ini = 0;
+	int i, j=0;
+	char *opcao = (char*)malloc(sizeof(char));
+	Aluno **aprovados = (Aluno**)malloc(TAMANHO_INICIAL_LISTA*sizeof(Aluno*));
+
+	for (i=0; i<lista_->quantidade; i++) {
+		if(estaAprovado(&lista_->aluno[i])) {
+			aprovados[j] = &lista_->aluno[i];
+			j++;
+		}
+	}
+
+	while (1) {
+
+		clearScreen();
+		imprimeListaAprovados(aprovados, ini, j);
+		printf("Digite sua opcao: ");
+		scanf(" %[^\n]", opcao);
+
+		switch(*opcao) {
+
+		case '1':
+			ini -= 10;
+			if(ini < 0){
+				ini = 0;
+			}
+			break;
+
+		case '2':
+			if (ini+10 < lista_->quantidade) {
+				ini += 10;
+			}
+			break;
+
+		case '0':
+			free(opcao);
+			free(aprovados);
+			return;
+		}
+	}
 }
-void imprimeListaAprovados(){
+void imprimeListaAprovados(Aluno **aprovados, int ini, int max){
+
+	char *aux = (char*)malloc(80*sizeof(char));
+	int i;
+
+	cabecalho("Alunos aprovados");
+	line("Cod | Nome do aluno", 'L');
+	emptyLine();
+	for (i=ini; i<ini+10; i++) {
+		if(i>=max) {
+			emptyLine();
+		} else {
+			sprintf(aux, "%03d | %s", aprovados[i]->codigo, aprovados[i]->nome);
+			line(aux, 'L');
+		}
+	}
+	emptyLine();
+	emptyLine();
+	line("1 - Anterior | Proximo - 2", 'C');
+	line("0. Voltar", 'L');
+	emptyLine();
+	filledLine();
+
+	free(aux);
+}
+
+void menuReprovados(){
+
+	int ini = 0;
+	int i, j=0;
+	char *opcao = (char*)malloc(sizeof(char));
+	Aluno **reprovados = (Aluno**)malloc(TAMANHO_INICIAL_LISTA*sizeof(Aluno*));
+
+	for (i=0; i<lista_->quantidade; i++) {
+		if(!estaAprovado(&lista_->aluno[i])) {
+			reprovados[j] = &lista_->aluno[i];
+			j++;
+		}
+	}
+
+	while (1) {
+
+		clearScreen();
+		imprimeListaReprovados(reprovados, ini, j);
+		printf("Digite sua opcao: ");
+		scanf(" %[^\n]", opcao);
+
+		switch(*opcao) {
+
+		case '1':
+			ini -= 10;
+			if(ini < 0){
+				ini = 0;
+			}
+			break;
+
+		case '2':
+			if (ini+10 < lista_->quantidade) {
+				ini += 10;
+			}
+			break;
+
+		case '0':
+			free(opcao);
+			free(reprovados);
+			return;
+		}
+	}
+}
+void imprimeListaReprovados(Aluno **reprovados, int ini, int max){
+
+	char *aux = (char*)malloc(80*sizeof(char));
+	int i;
+
+	cabecalho("Alunos reprovados");
+	line("Cod | Nome do aluno", 'L');
+	emptyLine();
+	for (i=ini; i<ini+10; i++) {
+		if(i>=max) {
+			emptyLine();
+		} else {
+			sprintf(aux, "%03d | %s", reprovados[i]->codigo, reprovados[i]->nome);
+			line(aux, 'L');
+		}
+	}
+	emptyLine();
+	emptyLine();
+	line("1 - Anterior | Proximo - 2", 'C');
+	line("0. Voltar", 'L');
+	emptyLine();
+	filledLine();
+
+	free(aux);
+}
+
+
+// Funcoes de persistencia
+void lerDadosDoArquivo(char *filename) {
+
+	FILE *fp;
+	char *msg;
+	int i;
+
+	msg = malloc(80*sizeof(char));
+
+	fp = fopen(filename, "r");
+	if (!fp) {
+		sprintf(msg, "Nao foi possivel abrir %s para leitura.", filename);
+		setMensagemAviso(msg);
+		free(msg);
+		return;
+	}
+
+	fscanf(fp," %d-%d\n", &lista_->maximo, &lista_->quantidade);
+	for (i=0; i<lista_->quantidade; i++) {
+		fread(&lista_->aluno[i], sizeof(Aluno), 1, fp);
+	}
+	fclose(fp);
+
+	sprintf(msg, "Foram lidos %d alunos.", i);
+	setMensagemAviso(msg);
+	free(msg);
 
 }
+void salvarDadosNoArquivo(char* filename) {
 
+	FILE *fp;
+	char *msg;
+	int i;
+
+	msg = malloc(80*sizeof(char));
+
+	fp = fopen(filename, "w");
+	if (!fp) {
+		sprintf(msg, "Nao foi possivel abrir %s para escrita.", filename);
+		setMensagemAviso(msg);
+		free(msg);
+		return;
+	}
+
+	fprintf(fp,"%d-%d\n", lista_->maximo, lista_->quantidade);
+	for (i=0; i<lista_->quantidade; i++) {
+		fwrite(&lista_->aluno[i], sizeof(Aluno), 1, fp);
+	}
+	fclose(fp);
+
+	sprintf(msg, "Foram salvos %d alunos.", i);
+	setMensagemAviso(msg);
+	free(msg);
+}
 
 
 // Funcoes de mensagem
@@ -387,6 +623,7 @@ void imprimeMensagemAviso() {
 		emptyLine();
 	}
 }
+
 
 // Funcoes de debug
 Lista* criaListaFake() {
@@ -407,14 +644,14 @@ Lista* criaListaFake() {
 
 	return lista;
 }
-void imprimeLista(Lista* lista){
+void imprimeLista(Lista* lista) {
 
 	int i;
 
 	printf("Lista: %x\n", (unsigned int)lista);
 	printf("- Tamanho atual: %d\n", lista->quantidade);
 	printf("- Tamanho maximo: %d\n", lista->maximo);
-	printf("- Alunos: \n");
+	printf("- Aluno [%d]s: \n", lista->quantidade);
 	for (i=0; i<lista->quantidade; i++) {
 		imprimeAluno(&lista->aluno[i]);
 		printf("\n");
